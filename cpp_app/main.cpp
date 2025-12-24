fe1cdbec1840a4ff7507262e4d9faa2fb60f8f3e
SebastianGoldmann <sebastian.h.goldmann@outlook.com>
2025-12-24 13:40:33 +0100

    added initial cpp code and git ignore

--- cpp_app/main.cpp
+++ cpp_app/main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <algorithm> // Required for next_permutation
+#include <climits>   // Required for INT_MAX
+#include <cstdlib> // rand(), srand()
 
-int main() {
-    std::cout << "Hello from Ubuntu WSL!" << std::endl;
-    std::cout << "C++ project is successfully set up." << std::endl;
+using namespace std;
+
+
+// define number of cities
+const int V = 4;
+
+//create distance matrix
+int arr[V][V] = {0};
+
+
+// generate a random distance matrix
+int main() 
+{
+    srand(42);
+    cout << "Number of cities in simulation: " << V << endl;
+
+    cout << "Iniatlized empty matrix: " << endl;
+
+        for(int a= 0; a  < V; a++)
+        {
+                for(int b=0; b<V; b++)
+                {
+                    cout << arr[a][b] << " ";
+                }
+            cout << endl;
+        }
+
+    cout << "Generating random numbers" << endl;
+
+    for(int a=0; a< V; a++){
+        for(int b=0; b< V; b++){
+            arr[a][b] = rand() % 11;
+
+            cout << arr[a][b] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
+
+//distance matrix is now created for a specific number of cities
+
